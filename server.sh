c59b289beea982c7bbd582e789b1d7c1f0e01400
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2026-05-02 04:38:09 +0600

    script update

--- server.sh
+++ server.sh
@@ -1,1 +1,8 @@
-CUDA_VISIBLE_DEVICES=1 uvicorn main:app --port 5600 --host 0.0.0.0 > /dev/null 2>&1 &
+#!/bin/bash
+lsof -ti :5600 | xargs -r kill -9
+
+export CUDA_VISIBLE_DEVICES=1
+uvicorn main:app \
+  --port 5600 \
+  --host 0.0.0.0 \
+  --workers 1 > /dev/null 2>&1 &
